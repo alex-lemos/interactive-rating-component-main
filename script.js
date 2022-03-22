47df55a151e137dbfe0369a0b3cdd488d98001bb
Alex Lemos <alemosgomes@gmail.com>
2022-03-21 23:55:12 -0300

    add js

---
+++ script.js
@@ -0,0 +1,14 @@
+// GLOBAL
+
+let num = document.querySelectorAll(".rate__num");
+
+// EVENT
+
+num[0].addEventListener("click", modifyBg);
+
+// FUNCTION
+
+function modifyBg(){
+    // console.log(this);
+    console.log("teste");
+}
